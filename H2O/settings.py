9157641ed6357de29390b334b1c59d38b6145854
De. Mi. <me@denismilovanov.net>
2015-08-19 19:16:25 +0300

    Parsers

--- H2O/settings.py
+++ H2O/settings.py
@@ -77,6 +77,14 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    )
+}
+
 URL = 'http://localhost:8000'
 API_URL = URL
 

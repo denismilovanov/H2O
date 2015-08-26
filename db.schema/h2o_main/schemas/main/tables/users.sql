bac1503c177cfb104cfd5b3d1c58370bac2282d6
De. Mi. <me@denismilovanov.net>
2015-08-26 18:49:24 +0300

    main.users now have no sequence and default uuid

--- db.schema/h2o_main/schemas/main/tables/users.sql
+++ db.schema/h2o_main/schemas/main/tables/users.sql
@@ -2,8 +2,8 @@
 -- пользователи
 
 CREATE TABLE main.users (
-    id integer NOT NULL DEFAULT nextval('main.users_id_seq'::regclass) PRIMARY KEY,
-    uuid uuid NOT NULL UNIQUE DEFAULT public.uuid_generate_v4(),
+    id integer PRIMARY KEY,
+    uuid uuid NOT NULL UNIQUE,
     name varchar(255) NOT NULL,
     registered_at timestamptz NOT NULL DEFAULT now(),
     status main.user_status NOT NULL DEFAULT 'i_have_enough_money',

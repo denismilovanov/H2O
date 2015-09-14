ee5742671491d0b923c33a5f05044aea4cc86b8b
De. Mi. <me@denismilovanov.net>
2015-09-14 15:35:38 +0300

    Constraint on push token length

--- db.schema/h2o_main/schemas/main/tables/users_devices.sql
+++ db.schema/h2o_main/schemas/main/tables/users_devices.sql
@@ -9,3 +9,12 @@ CREATE TABLE main.users_devices (
 
     CONSTRAINT users_devices_pkey PRIMARY KEY(user_id, device_type, push_token)
 );
+
+-- ограничения на длину токена
+ALTER TABLE main.users_devices
+    ADD CONSTRAINT token_length CHECK (
+        CASE WHEN device_type = 'ios' AND push_token IS NOT NULL THEN length(push_token) = 64
+             WHEN device_type = 'android' AND push_token IS NOT NULL THEN length(push_token) = 152
+             ELSE TRUE
+        END
+    );

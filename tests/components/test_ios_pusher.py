346ddc1705ff4696cd518c33c8310bb25157cc67
De. Mi. <me@denismilovanov.net>
2015-09-25 15:40:58 +0300

    To previous

--- tests/components/test_ios_pusher.py
+++ tests/components/test_ios_pusher.py
@@ -7,7 +7,7 @@ from models import *
 class IOSPusherTestCase(SimpleTestCase):
     def test1(self):
         token = '89daa349b32514b539b035f12bf20c26ad0e2825512f18e3762b64da2454a022'
-        Pusher.get_pusher('ios').push(None, token, {'bla': 'text', 'inner': {'go_deeper': '1'}})
+        Pusher.get_pusher('ios').push(None, token, {'push_header': 'header', 'bla': 'text', 'inner': {'go_deeper': '1'}})
 
 
 
